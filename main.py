b82f1cb818138339e79d28b30d8c577c4254981d
Rudra <rudrapathak28@gmail.com>
2025-08-26 09:03:30 +0530

    Complete the Todo

--- main.py
+++ main.py
@@ -5,14 +5,20 @@ todos = functions.get_todos()
 
 def add_todo():
     new_todo = st.session_state['new_todo']+"\n"
+    st.session_state['new_todo'] = None
     todos.append(new_todo)
     functions.write_todos(todos)
 
 st.title('My Todos App')
 st.subheader('This is my Todo Web App')
 
-for todo in todos:
-    st.checkbox(label=todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(label=todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
 
 st.text_input(placeholder="Enter your todo...",
               label="",
